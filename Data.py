d1b45f51858d09e24487c37bb7c63e9efdd97c8e
Ian Mobbs <ianmobbs@gmail.com>
2017-04-22 15:25:04 -0500

    added tokens, cleaned up code

--- Data.py
+++ Data.py
@@ -1,10 +1,6 @@
 import csv
 import tweepy
-import nltk
-import pandas as pd
 from tokens import consumer_key, consumer_secret, access_token, access_token_secret
-from pprint import pprint
-import json
 
 def parseTweet(tweet):
     """
@@ -16,20 +12,26 @@ def parseTweet(tweet):
     # pprint(tweet)
     return [tweet['created_at'], tweet['user']['screen_name'], tweet['text']] 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
 
 # Announce date: 2017-03-29
 # Release date: 2017-04-21
 # Future date: 2017-04-22
 
-tweets = []
-query = "galaxy -rt s8"
-date1 = "2017-03-29"
-date2 = "2017-03-30"
-with open('results.csv', 'w') as f: 
-    w = csv.writer(f)
-    for tweet in tweepy.Cursor(api.search, q=query, since=date1, until=date2, lang="en").items():
-        w.writerow(parseTweet(tweet))
+def main():
+
+    # Tweepy setup
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # Query to search
+    query = "galaxy -rt s8"
+
+    # Writes CSV until rate limit hit
+    with open('results.csv', 'w') as f: 
+        w = csv.writer(f)
+        for tweet in tweepy.Cursor(api.search, q=query, lang="en").items():
+            w.writerow(parseTweet(tweet))
+
+main()
---
+++ tokens.py
@@ -0,0 +1,4 @@
+consumer_key = "FcMJsG0YB5klhCP76XNqHNda3"
+consumer_secret = "l5ATpeMorxzRothUSGZHZQyiDUyet8NMALnfBLMMtmOWpI1daX"
+access_token = "577685529-UO4xE2Ez3k00CCqPqurY7Urz6kP1LxTxD99grHVC"
+access_token_secret = "xg4mWWS2erl42gMzoEqEc3gdzH0FusLAGUwk5svcoOUUa"
